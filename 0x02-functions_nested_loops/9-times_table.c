1d64a695682551278110f2f96d5ec7f7555d4815
AGbaby09 <agbotsem@gmail.com>
2023-09-13 14:51:13 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,11 +6,40 @@
 */
 void times_table(void)
 {
-	int x9 = 0;
+	int left = 0;
 
-	while (x9 < 82)
+	while (left < 10)
 	{
-		_putchar(x9 + '0');
-		x9 = x9 + 9;
+		int right = 0;
+
+		while (right < 10)
+		{
+			int o, t;
+			int product = left * right;
+
+			if (product >= 10)
+			{
+				t = product / 10;
+				o = product % 10;
+				_putchar(t + '0');
+				_putchar(o + '0');
+			}
+			else
+			{
+				_putchar(product + '0');
+			}
+
+			if (right == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			right++;
+		}
+		left++;
 	}
 }
