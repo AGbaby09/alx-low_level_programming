d4810a0dfab133e1864767b24614ab8ae78c0ee6
AGbaby09 <agbotsem@gmail.com>
2023-09-14 16:57:04 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - check the code
+ * @n: parameter.
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int num = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}else
+	{
+		while (num < n)
+		{
+			_putchar('_');
+			num++;
+		}
+		_putchar('\n');
+	}
+}
