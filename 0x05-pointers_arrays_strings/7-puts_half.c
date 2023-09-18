0605764269d2c70b79d340b0e1b2ecaac7b5f159
AGbaby09 <agbotsem@gmail.com>
2023-09-18 13:06:42 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - check the code.
+ * @str: parameter.
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int count = 0;
+	int half;
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	if (count % 2 == 1)
+	{
+		half = (count - 1) / 2;
+	}
+	else
+	{
+		half = count / 2;
+	}
+
+	while (str[half] != '\0')
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
