b5c63dbbc52101d729547f3b4b4a5c3fd54a3436
AGbaby09 <agbotsem@gmail.com>
2023-09-22 06:43:21 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - check the code
+ * @dest: param
+ * @src: param
+ * @n: param
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	while (src[b] != '\0' && b < n)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
