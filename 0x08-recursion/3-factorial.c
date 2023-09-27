0dd9888a4db598100f4ebee8aadb52a1446d0fc4
AGbaby09 <agbotsem@gmail.com>
2023-09-27 09:36:31 -0700

    0x08

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - function
+ * @n: param
+ * Return: value
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
