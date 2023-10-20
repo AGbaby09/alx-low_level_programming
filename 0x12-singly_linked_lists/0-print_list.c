958b9409ebeed126f414b1edd0777330643e9e4e
AGbaby09 <agbotsem@gmail.com>
2023-10-19 18:56:08 -0700

    0x12

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - ...
+ *
+ * @h: ...
+ * Return: ...
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
