fd253b4583bb25b454e15bd1d349d40b67febf97
AGbaby09 <agbotsem@gmail.com>
2023-10-19 19:02:36 -0700

    commit qai

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,16 @@
-#include "lists.h"
-
+#include "list.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 /**
- * print_list - ...
- *
- * @h: ...
- * Return: ...
+ * print_list - prints a list
+ * @h: head of list
+ * Return: number of elements
  */
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	int count = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -21,7 +18,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		count++;
 	}
-	return (i);
+	return (count);
 }
